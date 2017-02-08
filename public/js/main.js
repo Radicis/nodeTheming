e260d31fbce945e2f90270b202af070a4ab0ee9f
Adam <alloyddesign@gmail.com>
2017-02-08 11:54:43 +0000

    look and feel

--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,11 @@ $(document).ready(function() {
     var height = movementStrength / $(window).height();
     var width = movementStrength / $(window).width();
     $("body").mousemove(function(e){
-        var pageX = e.pageX - ($(window).width() / 2);
+        var pageX = e.pageX - (($(window).width()+300) / 2);
         var pageY = e.pageY - ($(window).height() / 2);
-        var newvalueX = width * pageX * -1 - 25;
-        var newvalueY = height * pageY * -1 - 50;
-        console.log($('#full-screen').css('background-position'));
-        $('#full-screen').css("top", newvalueX*2+"px")
-        $('#full-screen').css("left", newvalueX*2+"px");
+        var newvalueX = width * pageX * -2 - 25;
+        var newvalueY = height * pageY * -2 - 50;
+        $('#full-screen').css("top", newvalueY+"px")
+        $('#full-screen').css("left", newvalueX+"px");
     });
 });
