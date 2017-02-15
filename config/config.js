d96d88008fc638a90dab3fc816c80353315458e6
Adam <alloyddesign@gmail.com>
2017-02-15 22:06:33 +0000

    Added post route and simplified further

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     database: 'mongodb://localhost/tate',
     port: process.env.PORT || 3030, // used to create, sign, and verify tokens
 
-    collectionName: "movies"
+    collectionName: "artworks"
 
 
 };
--- routes/config.js
+++ routes/config.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res) {
     res.render('config', {});
 });
 
-router.post('/',function(req,res){
+router.post('/',function(req, res, next){
 
     var newSchema = {
         collectionName : req.body.collectionName,
@@ -41,6 +41,8 @@ router.post('/',function(req,res){
     console.log(newSchema);
     console.log(customFields);
 
+    var schema = {};
+
     DisplaySchema.add(newSchema, function(err, schema){
         customFields.forEach(function(field){
             console.log("Trying to add: " + field.label);
@@ -48,10 +50,21 @@ router.post('/',function(req,res){
                 console.log("Field added: " + createdField.label);
             });
         });
+        res.render('config', {schema: schema});
     });
 
-    res.render('config', {});
+});
 
+
+router.get('/:_id', function(req, res){
+    var id = req.params._id;
+    var context = {};
+    DisplaySchema.getById(id, function(err, schema){
+        if(err)
+            throw err;
+        context.schema = schema;
+        res.render('config', context);
+    })
 });
 
 module.exports = router;
