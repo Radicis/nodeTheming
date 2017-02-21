3d2ace8b4c5af63f38455094d31a8a1a211c5379
Adam Lloyd <Adam Lloyd>
2017-02-21 21:32:23 +0000

    moved to json implementation

--- routes/index.js
+++ routes/index.js
@@ -2,50 +2,16 @@ var express = require('express');
 var router = express.Router();
 var helpers = require('../middleware/helpers');
 var config = require('../config/config');
-var DisplaySchema = require('../models/displaySchema');
-// Connect to MongoDb
-var mongoose = require('mongoose');
-var db = mongoose.connection;
+var request = require('request');
 
 router.get('/', function(req, res) {
 
-    var collection = config.collectionName;
-
-    var context = {};
-
-    DisplaySchema.getByCollectionName(collection, function(err, displaySchema) {
-        var collection = db.collection(displaySchema.collectionName);
-
-        collection.find({}, {limit:4}).toArray(function(error, objects) {
-
-            console.log("Hello");
-            if (err) {
-                throw err;
-            }
-
-            context.objects = [];
-
-            objects.forEach(function(object){
-
-                var fooObject = {
-                    title: object[displaySchema.title],
-                    thumbnail: object[displaySchema.thumbnail],
-                    url: object[displaySchema.url],
-                    date: object[displaySchema.date],
-                    customFields: []
-                };
-
-                displaySchema.customFields.forEach(function (field) {
-                    fooObject.customFields.push({label: field.label, ref: object[field.ref]});
-                });
-
-                fooObject.bg = object[displaySchema['thumbnail']];
-
-                context.objects.push(fooObject);
-            });
-
+    request('http://localhost:3030/object/0', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var context = {};
+            context.objects = JSON.parse(body);
             try {
-                helpers.getImageColours(context.objects[0].thumbnail).then(function (colours) {
+                helpers.getImageColours(context.object[0].thumbnail).then(function (colours) {
                     context.colours = colours;
                     res.render('default', context);
                 });
@@ -54,8 +20,7 @@ router.get('/', function(req, res) {
                 console.log("Vibrant Crashed!: " + e);
                 res.render('default', context);
             }
-
-        });
+        }
     });
 });
 
---
+++ routes/object.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var router = express.Router();
+var helpers = require('../middleware/helpers');
+var config = require('../config/config');
+var DisplaySchema = require('../models/displaySchema');
+// Connect to MongoDb
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+
+router.get('/random', function(req, res) {
+
+    var context = {};
+
+    DisplaySchema.getByCollectionName(config.collectionName, function(err, displaySchema) {
+        if (err) {
+            throw err;
+        }
+
+        var collection = db.collection(displaySchema.collectionName);
+        var count = 30000;
+        var rand = Math.floor(Math.random() * count);
+        collection.find({thumbnail: {$ne: ""}}, {limit:1}).skip(rand).toArray(function(error, objects) {
+            if (err) {
+                throw err;
+            }
+
+            var object = objects[0];
+
+            context.object = {
+                title: object[displaySchema.title],
+                thumbnail: object[displaySchema.thumbnail],
+                url: object[displaySchema.url],
+                date: object[displaySchema.date],
+                customFields: []
+            };
+
+            displaySchema.customFields.forEach(function (field) {
+                context.object.customFields.push({label: field.label, ref: object[field.ref]});
+            });
+
+            context.object.bg = object[displaySchema['thumbnail']];
+
+            res.json(context.object);
+        });
+    });
+});
+
+router.get('/:count', function(req, res) {
+
+
+    DisplaySchema.getByCollectionName(config.collectionName, function(err, displaySchema) {
+        if (err) {
+            throw err;
+        }
+
+        var count = parseInt(req.params.count);
+
+        var collection = db.collection(displaySchema.collectionName);
+        collection.find({}, {limit:10}).skip(count).toArray(function(error, dbObjects) {
+            if (err) {
+                throw err;
+            }
+
+            var objects = [];
+
+            dbObjects.forEach(function(dbObject){
+
+                var object = {
+                    title: dbObject[displaySchema.title],
+                    thumbnail: dbObject[displaySchema.thumbnail],
+                    url: dbObject[displaySchema.url],
+                    date: dbObject[displaySchema.date],
+                    customFields: []
+                };
+
+                displaySchema.customFields.forEach(function (field) {
+                    object.customFields.push({label: field.label, ref: dbObject[field.ref]});
+                });
+
+                object.bg = dbObject[displaySchema['thumbnail']];
+
+                objects.push(object)
+            });
+
+            res.json(objects);
+        });
+    });
+});
+
+router.get('/setup', function(err, data){
+    helpers.createDummyData().then(function(field){
+        console.log("Dummy Data created!");
+    });
+});
+
+module.exports = router;
